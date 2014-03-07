9956219c1802e666264da7ff6ddbb18b5a5d2dd1
Rob Muhlestein <rob@skilstak.com>
2014-03-07 15:50:41 -0500

    added body padding

--- src/scripts/thumble.js
+++ src/scripts/thumble.js
@@ -4,6 +4,15 @@ var thumble = document.getElementById('thumble');
 var main = document.getElementById('thumble-main');
 var btns = document.getElementById('thumble-btns');
 var pad = document.getElementById('thumble-pad');
+var body = document.getElementsByTagName('body')[0];
+
+// TODO add inferred sizes of the main thumble elements
+// rather than requiring they be hard coded in the CSS
+
+if (typeof main.className == 'undefined') main.className = '';
+if (typeof btns.className == 'undefined') btns.className = '';
+if (typeof pad.className == 'undefined') pad.className = '';
+if (typeof body.className == 'undefined') body.className = '';
 
 if (thumble.addEventListener) {
   main.addEventListener("click", toggleThumblePad, false);
@@ -13,16 +22,17 @@ if (thumble.addEventListener) {
   }
 }
 
-
 function toggleThumblePad(click) {
   if (main.className.indexOf("off") >= 0) {
-    main.className = '';
-    btns.className = '';
-    pad.className = '';
+    main.className = main.className.replace(/off/g, '');
+    btns.className = btns.className.replace(/off/g, '');
+    pad.className = pad.className.replace(/off/g, '');
+    body.className = body.className.replace(/off/g, '');
   } else {
-    main.className = 'off';
-    btns.className = 'off';
-    pad.className = 'off';
+    main.className += 'off';
+    btns.className += 'off';
+    pad.className += 'off';
+    body.className += 'off';
   }
 }
 
