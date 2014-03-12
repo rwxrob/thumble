c75e005725eeb7120415e006c5fa9b6d949dba74
Rob Muhlestein <rob@skilstak.com>
2014-03-11 23:15:51 -0400

    javascript toggle working

--- scripts/thumble.js
+++ scripts/thumble.js
@@ -7,29 +7,31 @@ if (typeof main.className == 'undefined') main.className = '';
 if (typeof thumble.className == 'undefined') pad.className = '';
 
 if (main.addEventListener) {
-  main.addEventListener("click", showThumblePad, false);
+  main.addEventListener("click", showThumble, false);
 } else {
   if (main.attachEvent) {
-    main.attachEvent("click", showThumblePad);
+    main.attachEvent("click", showThumble);
   }
 }
 
 if (thumble.addEventListener) {
-  thumble.addEventListener("click", hideThumblePad, false);
+  thumble.addEventListener("click", hideThumble, false);
 } else {
   if (thumble.attachEvent) {
-    thumble.attachEvent("click", hideThumblePad);
+    thumble.attachEvent("click", hideThumble);
   }
 }
 
-function hideThumblePad(click) {
+function hideThumble(click) {
+  main.className = main.className.replace('off', '');
+  //main.className = '';
   thumble.className += 'off';
-  main.className.replace(/off/g, '');
 }
 
-function showThumblePad(click) {
+function showThumble(click) {
     main.className += 'off';
-    thumble.className.replace(/off/g, '');
+    //thumble.className = '';
+    thumble.className = thumble.className.replace('off', '');
 }
 
 })();
