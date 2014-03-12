b47fac1bf03e22114281a0e61852764eaf2d582a
Rob Muhlestein <rob@skilstak.com>
2014-03-11 22:23:25 -0400

    eventhandler working

--- src/scripts/thumble.js
+++ src/scripts/thumble.js
@@ -1,32 +1,35 @@
 (function(){
 
-var thumble = document.getElementById('thumble');
 var main = document.getElementById('thumble-main');
-var btns = document.getElementById('thumble-btns');
-var pad = document.getElementById('thumble-pad');
+var thumble = document.getElementById('thumble');
 
 if (typeof main.className == 'undefined') main.className = '';
-if (typeof btns.className == 'undefined') btns.className = '';
-if (typeof pad.className == 'undefined') pad.className = '';
+if (typeof thumble.className == 'undefined') pad.className = '';
 
-if (thumble.addEventListener) {
-  main.addEventListener("click", toggleThumblePad, false);
+if (main.addEventListener) {
+  main.addEventListener("click", showThumblePad, false);
 } else {
   if (main.attachEvent) {
-    main.attachEvent("click", toggleThumblePad);
+    main.attachEvent("click", showThumblePad);
   }
 }
 
-function toggleThumblePad(click) {
-  if (main.className.indexOf("off") >= 0) {
-    main.className = main.className.replace(/off/g, '');
-    btns.className = btns.className.replace(/off/g, '');
-    pad.className = pad.className.replace(/off/g, '');
-  } else {
+if (thumble.addEventListener) {
+  thumble.addEventListener("click", hideThumblePad, false);
+} else {
+  if (thumble.attachEvent) {
+    thumble.attachEvent("click", hideThumblePad);
+  }
+}
+
+function hideThumblePad(click) {
+  thumble.className += 'off';
+  main.className.replace(/off/g, '');
+}
+
+function showThumblePad(click) {
     main.className += 'off';
-    btns.className += 'off';
-    pad.className += 'off';
-  } 
+    thumble.className.replace(/off/g, '');
 }
 
 })();
